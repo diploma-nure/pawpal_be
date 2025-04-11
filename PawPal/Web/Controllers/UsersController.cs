8b95654d94541581b0cd63f8e1ec731c0d7306ea
Volodymyr Fedorenko <volodymyr.fedorenko@nure.ua>
2025-04-11 22:09:01 +0300

    Add auth to controllers

--- PawPal/Web/Controllers/UsersController.cs
+++ PawPal/Web/Controllers/UsersController.cs
@@ -7,10 +7,12 @@ public class UsersController(IMediator mediator) : ControllerBase
     private readonly IMediator _mediator = mediator;
 
     [HttpGet("info")]
+    [Auth]
     public async Task<Result<UserInfoDto>> GetUserInfoAsync([FromQuery] GetUserInfoQuery query, CancellationToken cancellationToken)
         => new(await _mediator.Send(query, cancellationToken));
 
     [HttpGet("pets/liked")]
+    [Auth]
     public async Task<Result<List<PetInListDto>>> GetLikedPetsAsync(CancellationToken cancellationToken)
         => new(await _mediator.Send(new GetLikedPetsQuery(), cancellationToken));
 }
