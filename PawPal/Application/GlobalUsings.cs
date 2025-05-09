4586d2f0524dd06502c7d8b73a3c6fb6e17c4928
Volodymyr Fedorenko <volodymyr.fedorenko@nure.ua>
2025-05-09 21:33:15 +0300

    Add pet recommendation functionality

--- PawPal/Application/GlobalUsings.cs
+++ PawPal/Application/GlobalUsings.cs
@@ -33,6 +33,7 @@ global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Options;
 global using Microsoft.IdentityModel.Tokens;
+global using System.ComponentModel.DataAnnotations.Schema;
 global using System.Globalization;
 global using System.IdentityModel.Tokens.Jwt;
 global using System.Reflection;
---
+++ PawPal/Application/Modules/Pets/Queries/GetRecommendedPetsQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Modules.Pets.Queries;
+
+public sealed class GetRecommendedPetsQueryValidator
+    : AbstractValidator<GetRecommendedPetsQuery>
+{
+    public GetRecommendedPetsQueryValidator()
+    {
+        RuleFor(query => query.Pagination)
+            .NotNull()
+            .SetValidator(new PaginationDtoValidator());
+    }
+}
