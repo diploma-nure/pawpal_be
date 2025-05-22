85abc04f39242780489ed82a13a36b8e7261a3cf
Volodymyr Fedorenko <volodymyr.fedorenko@nure.ua>
2025-05-22 12:07:55 +0300

    Add background job to update meeting statuses

---
+++ PawPal/Application/Services/MeetingBackgroundService.cs
@@ -0,0 +1,31 @@
+﻿namespace Application.Services;
+
+public class MeetingBackgroundService(IServiceScopeFactory scopeFactory, ILogger<MeetingBackgroundService> logger) : BackgroundService
+{
+    private readonly IServiceScopeFactory _scopeFactory = scopeFactory;
+
+    private readonly ILogger<MeetingBackgroundService> _logger = logger;
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation("Executing update of meetings statuses");
+
+            using var scope = _scopeFactory.CreateScope();
+            var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+
+            try
+            {
+                var count = await mediator.Send(new UpdateMeetingStatusesCommand(), stoppingToken);
+                _logger.LogInformation("Updated {count} meetings", count);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Exception occurred while updating meeting statuses: {message}", ex.Message);
+            }
+
+            await Task.Delay(Constants.TimePeriods.MeetingStatusCheck, stoppingToken);
+        }
+    }
+}
